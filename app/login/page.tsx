bbd6a106d93b8a42c14db917841abc8e694488d5
Pariyawit Jatukannyaprateep <pariyawit.jat@gmail.com>
2025-08-20 20:18:46 +1000

    Setup Next and Chakra

---
+++ app/login/page.tsx
@@ -0,0 +1,11 @@
+import { HStack, Button } from '@chakra-ui/react';
+
+const LoginPage = () => {
+  return (
+    <HStack>
+      <Button>Click me</Button>
+      <Button>Click me</Button>
+    </HStack>
+  );
+};
+export default LoginPage;
